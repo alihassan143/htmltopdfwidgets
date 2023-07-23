250aada3a1a1a0098e741c91fede8a78f534e0ae
alihassan143 <alihassan143cool@gmail.com>
2023-07-23 11:44:47 +0500

    chore: issue templates added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,12 +1,48 @@
 import 'dart:io';
 
-import 'package:htmltopdfwidgets/htmltopdfwidgets.dart';
+import 'package:flutter/material.dart';
+import 'package:htmltopdfwidgets/htmltopdfwidgets.dart' as htmltopdfwidgets;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
-  createDocument();
+  runApp(const MyApp());
 }
 
-const htmlText = '''  <h1>Heading Example</h1>
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final htmlText = '''<h1>Heading Example</h1>
   <p>This is a paragraph.</p>
   <img src="image.jpg" alt="Example Image" />
   <blockquote>This is a quote.</blockquote>
@@ -14,17 +50,45 @@ const htmlText = '''  <h1>Heading Example</h1>
     <li>First item</li>
     <li>Second item</li>
     <li>Third item</li>
-  </ul>"''';
+  </ul>
+ ''';
 
-createDocument() async {
-  var filePath = 'example.pdf';
-  var file = File(filePath);
-  final newpdf = Document();
-  List<Widget> widgets = await HTMLToPdf().convert(htmlText);
-  newpdf.addPage(MultiPage(
-      maxPages: 200,
-      build: (context) {
-        return widgets;
-      }));
-  await file.writeAsBytes(await newpdf.save());
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Html To Pdf"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: () {
+                  createDocument();
+                },
+                child: const Text("Create Pdf"))
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  createDocument() async {
+    var directory = await getApplicationDocumentsDirectory();
+    var filePath = '${directory.path}/example.pdf';
+    var file = File(filePath);
+    final newpdf = htmltopdfwidgets.Document();
+    List<htmltopdfwidgets.Widget> widgets =
+        await htmltopdfwidgets.HTMLToPdf().convert(htmlText);
+    newpdf.addPage(htmltopdfwidgets.MultiPage(
+        maxPages: 200,
+        build: (context) {
+          return widgets;
+        }));
+    await file.writeAsBytes(await newpdf.save());
+    print('File created: $filePath');
+  }
 }
