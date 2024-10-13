4b3f010f7d33acdc0f7908c4119ba78c02bd9576
Daniel Iwanicki <daniel.iwanicki@stxnext.pl>
2024-10-13 05:25:25 +0200

    Fixed font size

--- lib/src/html_to_widgets.dart
+++ lib/src/html_to_widgets.dart
@@ -213,6 +213,13 @@ class WidgetsHTMLDecoder {
   Future<List<Widget>> _parseTable(dom.Element element) async {
     final List<TableRow> tableRows = [];
 
+    return [
+      Table(
+          border: TableBorder.all(color: PdfColors.black),
+          children: []
+      ),
+    ];
+
     dom.Element tbody = element.children.first;
 
     for (final child in tbody.children)
